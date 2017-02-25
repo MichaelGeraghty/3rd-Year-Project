f505cbc70f6eab25a6d175e8a429b938807150be
MichaelGeraghty <geraghtymichael95@gmail.com>
2017-02-25 15:40:34 +0000

    Blog posts database basics set up with a basic home page and new post page able to view and post new entries

---
+++ project/app.py
@@ -0,0 +1,43 @@
+#imports
+from flask import Flask, render_template, request, redirect, url_for
+from models import *
+
+app = Flask(__name__)
+
+#before request to initialise and connects the blog db before it is requested
+@app.before_request
+def before_request():
+	initialise_db()
+
+#wether it is succesful or fails connecting to the db it will close and displays exceptions if fails
+@app.teardown_request
+def teardown_request(exception):
+	db.close()
+
+#root dir with the date format to be rendered in home.html
+@app.route('/')
+def home():
+#orders posts by latest .desc
+	return render_template('home.html',posts=Post.select().order_by(Post.date.desc()))
+
+#for creating a new post
+@app.route('/new_post/')
+def new_post():
+	return render_template('new_post.html')
+
+#To post into the database
+#only allows a post method not accessable from users
+@app.route('/create/', methods=['POST'])
+def create_post():
+	#creates the new post
+	Post.create(
+		title=request.form['title'],
+		text=request.form['text']
+	)
+	#allows the redirection back to home even if '/' is replaced ie will always redirect home
+	return redirect(url_for('home'))
+
+
+#running app using default port 5000
+if __name__== '__main__':
+	app.run(debug=True)
---
+++ project/models.py
@@ -0,0 +1,30 @@
+#imports
+from peewee import *
+import datetime
+
+#blog posts database using peewee sqlite
+
+
+#db = SqliteDatabase('posts.db')
+#entries db with two test entries
+db = SqliteDatabase('entries.db')
+
+class Post(Model):
+	id = PrimaryKeyField()
+	#date will be presented at the point of submission to database so date will be truely representitive of actual submission
+	date = DateTimeField(default = datetime.datetime.now)
+	#could have unique=true titles may add feature in future but need to add warnings at time of creation to avoid post being lost, not essencial
+	#need to set max lengths for titles and posts and prevent null/empty entries
+	title = CharField()
+	#text field for the post being exponentially larger
+	text = TextField()
+	#specifies the db created
+	class Meta:
+		database = db
+
+
+#initialise the blog database to connect and create the post table
+def initialise_db():
+	db.connect()
+	db.create_tables([Post], safe=True)
+	
